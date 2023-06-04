2db9f485efdeb1c7a753ca0af9aa27716d0cb17e
davidkilamlya <dkilamlya@outlook.com>
2023-06-04 12:08:56 +0300

    image banner slider added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+// import { Slider } from "./Components/NavBar/Slider";
 import { NavBar } from "./Components/NavBar/NavBar";
+import Slider from "./Components/NavBar/Slider";
 import Header from "./Container/Header/Header";
-
+import { images } from "./Constants";
+const imgs = [images.banner1, images.banner2, images.banner3];
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-     <Header/>
+      <NavBar />
+      <Slider slides={imgs} />
+      <Header />
     </div>
   );
 }
--- src/Constants/images.js
+++ src/Constants/images.js
@@ -1,5 +1,11 @@
 import logo from "../Assets/busteki_official (7).png";
+import banner1 from "../Assets/banner1.png";
+import banner2 from "../Assets/banner2.png";
+import banner3 from "../Assets/banner3.png";
 
 export default {
   logo,
+  banner1,
+  banner2,
+  banner3,
 };
